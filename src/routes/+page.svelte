4681a5e73d94be840614e108d233b229d771951e
Kishan Jobanputra <kishanj918@gmail.com>
2025-03-13 09:53:36 +0530

    feat: Add create table button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-yey
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Plus from '$lib/icons/Plus.svelte';
+</script>
+
+<main class="flex h-screen items-center justify-center">
+	<Plus />
+	<Button variant="brand" size="sm">Create New Table</Button>
+</main>
